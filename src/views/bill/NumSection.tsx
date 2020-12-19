8bf46f1ea1e0566ceae7ed69c2b75f60fea0961e
SuperPowerZhang <2285546178@qq.com>
2020-12-19 22:36:20 +0800

    extract calculate function and NumWrapper from NumSection

--- src/views/bill/NumSection.tsx
+++ src/views/bill/NumSection.tsx
@@ -1,133 +1,28 @@
-import styled from "styled-components";
 import React, {useState} from "react";
+import {NumWrapper} from './NumWrapper';
+import {GenerateNum} from './GenerateNum';
 
-const NumWrapper=styled.section`
-display: flex;
-flex-direction: column;
->div{
-text-align: right;
-font-size: 36px;
-padding: 10px ;
-background-color:rgb(255, 255, 255);
-box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-            inset 0 5px 5px -5px rgba(0,0,0,0.25);
-}
->.bill-num{
-display: grid;
-grid-template-rows:repeat(4,1fr);
-grid-template-columns:repeat(4,1fr) ;
-> li {
-padding-top:12px ;
-padding-bottom: 12px;
-font-size: 18px;
-line-height: 22px;
-text-align: center;
-background: #D3D3D3;
-vertical-align: middle;
-&:nth-child(1){
-background-color:#F2F2F2;
-}
-&:nth-child(2),
-&:nth-child(5){
-background-color: #E0E0E0;
-}
-&:nth-child(3),
-&:nth-child(6),
-&:nth-child(9){
-background-color:#D3D3D3;
-}
-&:nth-child(4),
-&:nth-child(7),
-&:nth-child(10){
-background-color:#C1C1C1;
-}
-&:nth-child(8),
-&:nth-child(11),
-&:nth-child(13){
-background-color:#B8B8B8;
-}
-&:nth-child(12){
-  grid-row: 3/5;
-  grid-column: 4/5;
-  padding-top: 35px;
-  padding-bottom: 35px;
-   background-color:#9A9A9A;
-}
-&:nth-child(13){
-  grid-row: 4/5;
-  grid-column: 1/3;
-}
-&:nth-child(14){
-background-color:#A9A9A9;
-}
-}
-}
-`;
 const NumSection:React.FC=()=>{
-    const [output,setOutPut]=useState<string>("0")
-    const getNumber=(e:React.MouseEvent)=>{
-        let num=(e.target as HTMLLIElement).innerText;
-        console.log(num);
-        switch (num){
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-            case '.':
-                if(output==='0'){
-                    if(num==='.'){
-                        setOutPut(output+num)
-                    }else{
-                        setOutPut(num)
-                    }
-                }else{
-                    setOutPut(output+num)
-                }
-                break;
-            case '删除':
-                if(output.length===1){
-                    setOutPut('0');
-                }else{
-                    setOutPut(output.slice(0,output.length-1));
-                }
-                break;
-            case '清空':
-                setOutPut('0');
-                break;
-            case 'OK':
-                console.log('OK');
-                break;
+    const [output,setOutPut]=useState<string>("0");
+    const getNumber=(text:string)=>{
+        if(text==='OK'){
+            //TODO
+            console.log('ok')
+        }else{
+            let outputNew=GenerateNum(text,output)||'0';
+            setOutPut(outputNew);
         }
-
-    };
+    }
+    const numbers:string[]=['1','2','3','删除','4','5','6','清空','7','8','9','OK','0','.'];
     return(
         <NumWrapper>
             <div>
                 {output}
             </div >
-            <ul className="bill-num" onClick={(e)=>{
-                getNumber(e)
-            }}>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>删除</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
-                <li>清空</li>
-                <li>7</li>
-                <li>8</li>
-                <li>9</li>
-                <li>OK</li>
-                <li>0</li>
-                <li>.</li>
+            <ul className="bill-num">
+                {numbers.map(num=>{
+                    return <li key={num} onClick={()=>getNumber(num)}>{num}</li>
+                })}
             </ul>
         </NumWrapper>
     )
