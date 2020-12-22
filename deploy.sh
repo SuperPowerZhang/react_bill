c431ad547cf5dae2d6f2a70e965a86a34b54b12a
SuperPowerZhang <2285546178@qq.com>
2020-12-22 21:04:01 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
-cd ..
-git add .
-git commit -m "update"
-git push
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git remote add origin git@github.com:SuperPowerZhang/react_bill_website.git &&
+git push -u -f origin master &&
+git remote add gitee git@gitee.com:tammiezhang/react_bill_website.git &&
+git push -u -f gitee master &&
+cd .. &&
+echo https://github.com/SuperPowerZhang/react_bill_website
+echo
